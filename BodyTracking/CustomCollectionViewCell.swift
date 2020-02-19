75168d4133406ba3366e03101d68e1afcafad224
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-02-19 15:10:51 -0300

    Adicionado o seletor de partes do corpo

---
+++ BodyTracking/CustomCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCollectionViewCell.swift
+//  BodyTracking
+//
+//  Created by Zewu Chen on 19/02/20.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- BodyTracking/ViewController.swift
+++ BodyTracking/ViewController.swift
@@ -10,8 +10,9 @@ import RealityKit
 import ARKit
 import Combine
 
-class ViewController: UIViewController, ARSessionDelegate {
 
+class ViewController: UIViewController, ARSessionDelegate {
+    
     @IBOutlet var arView: ARView!
     
     var character: BodyTrackedEntity?
@@ -20,28 +21,57 @@ class ViewController: UIViewController, ARSessionDelegate {
     var counter = 0
     let boxAnchor = AnchorEntity()
     var headAnchor = AnchorEntity()
+    var leftHandAnchor = AnchorEntity()
+    var rightHandAnchor = AnchorEntity()
+    var rightFootAnchor = AnchorEntity()
+    var leftFootAnchor = AnchorEntity()
+    var cells: [String] = ["cabeça", "mão-direita", "mão-esquerda", "pé-direito", "pé-esquerdo"]
+    var CUSTOMCELL = "CustomCollectionViewCell"
     
     var plusz: Float = 0
     var reancor = false
+    
+    let defauts = UserDefaults()
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        slider.minimumValue = 0.0
+        slider.maximumValue = 1.0
+        
+        slider.value = defauts.float(forKey: "Color")
+        let color = UIColor(hue: CGFloat(defauts.float(forKey: "Color")), saturation: 1, brightness: 1, alpha: 1)
+        slider.tintColor = color
+        
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: CUSTOMCELL, bundle: nil), forCellWithReuseIdentifier: CUSTOMCELL)
+        collectionView.delegate = self
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         arView.session.delegate = self
         let transform = self.arView.cameraTransform
         headAnchor.transform = transform
-
+        
         
         guard ARBodyTrackingConfiguration.isSupported else {
             fatalError("This feature is only supported on devices with an A12 chip")
         }
-
+        
         let configuration = ARBodyTrackingConfiguration()
         arView.session.run(configuration)
         arView.scene.addAnchor(boxAnchor)
         arView.scene.addAnchor(characterAnchor)
         arView.scene.addAnchor(headAnchor)
-//        arView.scene.addAnchor(boxAnchor)
-
+        arView.scene.addAnchor(leftFootAnchor)
+        arView.scene.addAnchor(rightFootAnchor)
+        arView.scene.addAnchor(leftHandAnchor)
+        arView.scene.addAnchor(rightHandAnchor)
+        
         var cancellable: AnyCancellable? = nil
         cancellable = Entity.loadBodyTrackedAsync(named: "character/robot").sink(
             receiveCompletion: { completion in
@@ -61,21 +91,16 @@ class ViewController: UIViewController, ARSessionDelegate {
         })
     }
     
-    @IBAction func didTapButton(_ sender: Any) {
+    func moveBoxToAnchor(_ anchor: AnchorEntity) {
         for child in boxAnchor.children {
-            headAnchor.addChild(child)
+            anchor.addChild(child)
         }
-//        headAnchor = boxAnchor.clone(recursive: true)
-        headAnchor.position = boxAnchor.position
-//        headAnchor = boxAnchor
-//        headAnchor = boxAnchor.clone(recursive: true)
-//        headAnchor.transform = boxAnchor.transform
+        anchor.position = boxAnchor.position
         boxAnchor.children.removeAll()
     }
     
     
     func addBox()  {
-//        boxAnchor = AnchorEntity()
         let color = UIColor(hue: CGFloat(UserDefaults.standard.float(forKey: "Color")), saturation: 1, brightness: 1, alpha: 1)
         let box = ModelEntity (
             mesh: MeshResource.generateBox(size: 0.1),
@@ -86,97 +111,79 @@ class ViewController: UIViewController, ARSessionDelegate {
         let location = SCNVector3(x: transform.matrix.columns.3.x, y: transform.matrix.columns.3.y, z: transform.matrix.columns.3.z)
         let plus =  SCNVector3Make(orientation.x + location.x, orientation.y + location.y, orientation.z + location.z)
         box.position = simd_make_float3(plus.x, plus.y, plus.z)
-//        self.arView.scene.anchors.append(box)
-//        boxAnchor.position = self.arView.cameraTransform.translation
-//        boxAnchor.position.z  -= 1
-//        arView.scene.anchors.append(box)
-//        arView.scene.addAnchor(box)
         if !reancor {
             plusz = plus.z
             boxAnchor.position = simd_make_float3(plus.x, plus.y, plus.z)
             reancor = true
         }
         boxAnchor.addChild(box)
-//        return boxAnchor
     }
-    
-//    func add(box: ModelEntity) {
-//
-//    }
     
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         for anchor in anchors {
             guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
-
+            
             // Update the position of the character anchor's position.
             let rightPosition = simd_make_float3(bodyAnchor.skeleton.modelTransform(for: .rightHand)!.columns.3)
             let headPosition = simd_make_float3(bodyAnchor.skeleton.modelTransform(for: .head)!.columns.3)
-
+            
             let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
-//            headAnchor.anchoring = .init(<#T##target: AnchoringComponent.Target##AnchoringComponent.Target#>)
             characterAnchor.position = bodyPosition
-//            headAnchor.setParent(character)
-//            headAnchor.setPosition(bodyPosition + headPosition, relativeTo: character)
-//          let tr = Transform(scale: [1.0, 1.0, 1.0], rotation: Transform(matrix: bodyAnchor.transform).rotation, translation: Transform(matrix: bodyAnchor.transform).translation)
-//            headAnchor.transform = .init(matrix: float4x4(columns: ))
             
             characterAnchor.addChild(headAnchor)
             headAnchor.position = bodyPosition + headPosition
             headAnchor.position.z += plusz
-
-//            headAnchor.transform = Transform(matrix: float4x4.ze)
-//            if boxAnchor.position == SIMD3<Float>.zero {
-//                boxAnchor.position = simd_make_float3(self.arView.cameraTransform.translation.x, self.arView.cameraTransform.translation.y, self.arView.cameraTransform.translation.z - 1)
-//            }
             
-
+            
             characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
             headAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
-            //            rightPosition.z *= characterAnchor.orientation.imag.y
-
-            //            boxAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
-
+            
             if let character = character, character.parent == nil {
                 characterAnchor.addChild(character)
-//                headAnchor.position = headAnchor.convert(position: bodyPosition + headPosition, to: character)
-//                boxAnchor.addChild(box)
             }
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        addBox()
-    }
-    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//            self.arView.scene.addAnchor(addBox())
         addBox()
     }
     
-
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        counter += 1
-    }
-    
-    @IBAction func btnSettings(_ sender: Any) {
-        let newVC = SettingsViewController(nibName: "SettingsViewController", bundle: nil)
-        newVC.modalPresentationStyle = .overCurrentContext
-        newVC.view.backgroundColor = .white
+    @IBAction func changedValueSlider(_ sender: Any) {
+        let color = UIColor(hue: CGFloat(slider.value), saturation: 1, brightness: 1, alpha: 1)
+        slider.tintColor = color
         
-        let navController = UINavigationController(rootViewController: newVC)
-        navController.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navController.navigationBar.shadowImage = UIImage()
-        navController.navigationBar.isTranslucent = true
-        navController.view.backgroundColor = .clear
-        
-        self.present(navController, animated: true, completion: nil)
+        defauts.set(CGFloat(slider.value), forKey: "Color")
     }
     
 }
 
-
-
-func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
-    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CUSTOMCELL, for: indexPath) as? CustomCollectionViewCell
+        cell?.image.image = UIImage(named: cells[indexPath.row])
+        return cell ?? CustomCollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            moveBoxToAnchor(headAnchor)
+        case 1:
+            moveBoxToAnchor(rightHandAnchor)
+        case 2:
+            moveBoxToAnchor(leftHandAnchor)
+        case 3:
+            moveBoxToAnchor(rightFootAnchor)
+        case 4:
+            moveBoxToAnchor(leftFootAnchor)
+        default:
+            break
+        }
+    }
+    
 }
