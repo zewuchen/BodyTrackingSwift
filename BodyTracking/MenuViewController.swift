8f3870ef202a054ee383ec2d1d6e34fca4339f25
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-02-20 14:03:20 -0300

    Finalizado Menu

---
+++ BodyTracking/MenuViewController.swift
@@ -0,0 +1,120 @@
+//
+//  MenuViewController.swift
+//  BodyTracking
+//
+//  Created by Zewu Chen on 19/02/20.
+//
+
+import UIKit
+import AVFoundation
+
+class MenuViewController: UIViewController {
+
+    var previewView : UIView!
+
+    //Camera Capture requiered properties
+    var videoDataOutput: AVCaptureVideoDataOutput!
+    var videoDataOutputQueue: DispatchQueue!
+    var previewLayer:AVCaptureVideoPreviewLayer!
+    var captureDevice : AVCaptureDevice!
+    let session = AVCaptureSession()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        previewView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+//        previewView.contentMode = UIView.ContentMode.scaleToFill
+        view.addSubview(previewView)
+        view.sendSubviewToBack(previewView)
+        self.setupAVCapture()
+    }
+
+    override var shouldAutorotate: Bool {
+        if (UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft ||
+        UIDevice.current.orientation == UIDeviceOrientation.landscapeRight ||
+        UIDevice.current.orientation == UIDeviceOrientation.unknown) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    @IBAction func btnGame(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let gameController = storyboard.instantiateViewController(withIdentifier: "GameViewController") as? ViewController else { return }
+        gameController.modalPresentationStyle = .fullScreen
+        
+        let navController = UINavigationController(rootViewController: gameController)
+        navController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navController.navigationBar.shadowImage = UIImage()
+        navController.navigationBar.isTranslucent = true
+        navController.modalPresentationStyle = .fullScreen
+        navController.view.backgroundColor = .clear
+        
+        self.present(navController, animated: true, completion: nil)
+    }
+    
+}
+
+extension MenuViewController:  AVCaptureVideoDataOutputSampleBufferDelegate{
+     func setupAVCapture(){
+        session.sessionPreset = AVCaptureSession.Preset.vga640x480
+        guard let device = AVCaptureDevice
+        .default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
+                 for: .video,
+                 position: AVCaptureDevice.Position.back) else {
+                            return
+        }
+        captureDevice = device
+        beginSession()
+    }
+
+    func beginSession(){
+        var deviceInput: AVCaptureDeviceInput!
+
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            guard deviceInput != nil else {
+                print("error: cant get deviceInput")
+                return
+            }
+
+            if self.session.canAddInput(deviceInput){
+                self.session.addInput(deviceInput)
+            }
+
+            videoDataOutput = AVCaptureVideoDataOutput()
+            videoDataOutput.alwaysDiscardsLateVideoFrames=true
+            videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
+            videoDataOutput.setSampleBufferDelegate(self, queue:self.videoDataOutputQueue)
+
+            if session.canAddOutput(self.videoDataOutput){
+                session.addOutput(self.videoDataOutput)
+            }
+
+            videoDataOutput.connection(with: .video)?.isEnabled = true
+
+            previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+
+            let rootLayer :CALayer = self.previewView.layer
+            rootLayer.masksToBounds=true
+            previewLayer.frame = rootLayer.bounds
+            rootLayer.addSublayer(self.previewLayer)
+            session.startRunning()
+        } catch let error as NSError {
+            deviceInput = nil
+            print("error: \(error.localizedDescription)")
+        }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // do stuff here
+    }
+
+    // clean up AVCapture
+    func stopCamera(){
+        session.stopRunning()
+    }
+
+}
